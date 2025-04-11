2a8063badf500e58bb33bf46093bb7459dd6df63
minnica <minnica20@gmail.com>
2025-04-10 22:05:40 -0600

    feat(*): instructions for exercice two

---
+++ todo-list.js
@@ -0,0 +1,18 @@
+// Crea una aplicación con un botón y una lista.
+// Al presionar el botón se debe de agregar un elemento a una lista desordenada con la siguiente leyenda "Task 1", "Task 2", "Task 3" y asi su
+// Solo puedes usar un método y una propiedad
+
+import { LitElement, html } from "lit";
+
+export class todoList extends LitElement {
+  static get properties() {}
+
+  constructor() {}
+
+  increment() {}
+
+  decrement() {}
+
+  render() {}
+}
+customElements.define("todo-list", todoList);
